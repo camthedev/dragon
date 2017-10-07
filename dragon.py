5820f3aa78828479f9b779e6527d290ddf414d7c
mcdowellstl <mcdowellstl@gmail.com>
2017-10-06 21:10:18 -0500

    modularized and cleaned up

--- dragon.py
+++ dragon.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
 
 #dictionary
-gold = "20"
-x = "5"
-y = "1"
-maxhp = "30"
-hp = "25"
+gold = 20
+max_x = 9
+max_y = 1
+x = 5
+y = 1
+maxhp = 30
+hp = 25
 fishingrod = "false"
 avatar = "&"
 location = "Fishing Hut"
 incombat = "false"
 fish = "false"
 dead = "false"
-# username = raw_input('Enter your name: ')
-username = "bob"
+username = raw_input('Enter your name: ')
+print username + " is your name"
 action = None
-
+#fix the dictionary
 # -----------------------
 
 def get_compass(x, y):
     # if x == "5" and y == "1":
     # this needs to handle different x/y coordinates and update string
-    compass_string = ".W..&...S"
-    return compass_string
+    compass_string = []
+    for num in range(1, max_x):
+      icon = "."
+      compass_string.append(icon)
+      # compass_string = ".W..&...S"
+      # [ ".", ".", "." ]
+
+    compass_string[x] == "&"
+    return ''.join(compass_string)
 
 def print_status():
     print "-------"
-    # print username + " is your name"
     print "%s    -   gold: %s    -   hp: %s:%s" % (get_compass(x,y), gold, hp, maxhp)
     print "You wake up in a forest, not knowing how you got there"
     print "To your left is a river, to your right is a hut"
@@ -51,7 +59,7 @@ def input_action():
         print "You take the rod and the gold."
         gold = "23"
         fishingrod = "true"
-
+#fix the status things
     elif action == "help":
         action_helper()
     elif action == "quit":
