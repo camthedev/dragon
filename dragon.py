e819468dd0ba2ac724cabfa0eccde443cd1ab278
mcdowellstl <mcdowellstl@gmail.com>
2017-10-09 19:43:47 -0500

    code cleanup

--- dragon.py
+++ dragon.py
@@ -26,12 +26,12 @@ class player:
     def move_player(self, direction, spaces):
         if direction == "left":
             self.x = self.x - 1
+            print "okay. moving " + direction + " " + str(spaces) + " spaces."
         elif direction == "right":
             self.x = self.x + 1
+            print "okay. moving " + direction + " " + str(spaces) + " spaces."
         else:
             print ("i can't move " + direction)
-
-
 
 
 class grid:
@@ -54,31 +54,23 @@ class grid:
     def clear_previous_location(self):
         self.grid_contents = ["." if gl=="&" else gl for gl in self.grid_contents]
 
+def action_goto(p,g,target):
+    print "Going to "+ target
+    print "It appears that "+ target + " is on fire. I recommend running."
 
 
 
-# fishingrod = False
-# location = "Fishing Hut"
-# incombat = False
-# fish = False
-# dead = False
-
-
-def clear():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-
-# def build_grid(max_x, max_y):
+# def clear():
+#     if os.name == 'nt':
+#         os.system('cls')
+#     else:
+#         os.system('clear')
 
 
 def print_status(p, g):
     # clear()
     print "-------"
     print "%s    -   gold: %s    -   hp: %s:%s" % (g.get_grid_string(p.x, p.y), p.gold, p.hp, p.maxhp)
-    print "You wake up in a forest, not knowing how you got there"
-    print "To your left is a river, to your right is a hut"
     print "-------"
 
 
@@ -97,23 +89,27 @@ def action_helper():
     # print "You could examine or use the hut and the river. Move quickly, you are getting hungry"
 
 
-def input_action(p):
+def input_action(p, g):
     action = None
     print "What do you do? (type 'help' for assistance)"
     full_action = raw_input()
     p.add_to_history(full_action)
+    # needs to handle a null input
 
     if not " " in full_action:
-        if full_action == "help":
+        if full_action in ["buy", "examine", "goto", "inventory", "move", "eat", "use"]:
+            print full_action + " what?"
+        elif full_action == "help":
             action_helper()
         elif full_action == "history":
             p.show_history()
         elif full_action == "quit":
             sys.exit(0)
+        else:
+            print "i don't know how to " + full_action
 
         raw_input('Press enter to continue: ')
         return action
-
 
     action_split = full_action.split()
 
@@ -125,37 +121,30 @@ def input_action(p):
     else:
         action = action_split[0]
         action_target = action_split[1]
-        print "action> " + action
-        print "target> " + action_target
-        print "==========="
 
     if action == "move":
         p.move_player(action_target, 1)
-
-    elif action == "examine hut":
+    elif action == "goto":
+        action_goto(p,g,action_target)
+    # elif action == "examine hut":
+    elif action == "examine":
         print "It's a fishing hut, with an old rod and some gold inside inside."
-    elif action == "use hut":
-      #  if not fishingrod:
+    # elif action == "use hut":
+    elif action == "use":
+        #if not fishingrod:
         print "You take the rod and the gold."
-        # gold + 3
-        #    fishingrod = True
+        self.gold + 3
+        self.fishingrod = True
      #   elif fishingrod:
     #        print "nothing left"
-    elif action == "examine river":
-        print "The river is flowing quite fast. You can see some fish inside"
-    elif action == "use river":
+    # elif action == "examine river":
+    #     print "The river is flowing quite fast. You can see some fish inside"
+    # elif action == "use river":
         #if fishingrod = "true"
             #print "You catch a fish"
             #fish = "true"
         #elif:
-        print "Not a good time to take a swim, bro"
-    elif action == "help":
-        action_helper()
-        raw_input('Press enter to continue: ')
-    elif action == "history":
-        p.show_history()
-    elif action == "quit":
-        sys.exit(0)
+        # print "Not a good time to take a swim, bro"
     else:
         print "i don't know how to " + action
         raw_input('Press enter to continue: ')
@@ -170,29 +159,16 @@ g = grid()
 
 username = raw_input('Enter your name: ')
 p.name = username
-print p.name + " is your name"
+print "Welcome to Dragon Quest, " + p.name
+print "As the game begins, you wake up in a forest, not knowing how you got there"
+print "To your left is a river, to your right is a hut"
 
 action = None
 
 while action != "quit":
     # clear()
     print_status(p, g)
-    action = input_action(p)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    action = input_action(p, g)
 
 
 
